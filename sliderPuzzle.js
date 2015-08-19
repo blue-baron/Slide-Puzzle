f6320e79dfb60f0396f85d9f0c988ab4b30ce28a
blue-baron <bluebaron75@gmail.com>
2015-08-19 22:23:58 +1000

    Add tileDimensions() method to setSlider.

--- sliderPuzzle.js
+++ sliderPuzzle.js
@@ -6,12 +6,46 @@ var setSlider = {
     sliderWidth: 0,
     sliderHeight: 0,
     
-    numTiles: 0,
+    numTiles: 9,
+    numRows: 0,
+    tileWidth: 0,
+    tileHeight: 0,
     
-    setImg: function(){
+    tilePositions: {},
+    
+    sliderDimensions: function(){
         this.sliderWidth = document.getElementById('sliderWrapper').clientWidth;
+        
         this.sourceImage = document.getElementById('imgSource');
-        },
-};
+        this.sourceImage.style.width = this.sliderWidth + "px";
+        
+        this.sliderHeight = this.sourceImage.clientHeight;
+    },
+    
+    tileDimensions: function () {
+        //determine tile dimensions
+        this.numRows = Math.sqrt(this.numTiles);
+        this.tileWidth = this.sliderWidth / this.numRows;
+        this.tileHeight = this.sliderHeight / this.numRows;
+        
+        //add tiles (divs) to slider
+        var slider = document.getElementById('sliderWrapper');
+        for (var i = 0; i < this.numTiles; i++) {
+            var newTile = document.createElement('div');
+            newTile.id = 'tile' + i;
+            newTile.className = 'sliderTile';
+            newTile.style.width = this.tileWidth + 'px';
+            newTile.style.height = this.tileHeight + 'px';
+            slider.appendChild(newTile);
+        }//end for
+         
+    }
+    
+   /* gridSetup: function () {
+            
+        }*/
+    
+};//end setSlider
 
-setSlider.setImg();
+setSlider.sliderDimensions();
+setSlider.tileDimensions();
