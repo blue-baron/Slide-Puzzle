db245d2e006bd546b7e2485879f26801697513ac
blue-baron <bluebaron75@gmail.com>
2015-08-20 21:36:17 +1000

    Adding method for setting up the puzzle grid. Still in progress...

--- sliderPuzzle.js
+++ sliderPuzzle.js
@@ -1,6 +1,6 @@
 /*jshint browser: true, devel: true*/
 
-var setSlider = {
+var slidePuzzle = {
     sourceImage: '',
     
     sliderWidth: 0,
@@ -11,41 +11,79 @@ var setSlider = {
     tileWidth: 0,
     tileHeight: 0,
     
-    tilePositions: {},
+    grid: {},
     
-    sliderDimensions: function(){
-        this.sliderWidth = document.getElementById('sliderWrapper').clientWidth;
+    puzzleSize: function(){
+        var puzzleWrapper = document.getElementById('puzzleWrapper'); 
+        //get width of puzzle
+        this.sliderWidth = puzzleWrapper.clientWidth;
         
+        //set puzzle img to puzzle width
         this.sourceImage = document.getElementById('imgSource');
         this.sourceImage.style.width = this.sliderWidth + "px";
         
+        //determine puzzle img height & set puzzle wrapper height
         this.sliderHeight = this.sourceImage.clientHeight;
+        puzzleWrapper.style.height = this.sliderHeight + 'px';
+        
     },
     
-    tileDimensions: function () {
-        //determine tile dimensions
+    createTiles: function () {
+        //determine number of rows in puzzle
         this.numRows = Math.sqrt(this.numTiles);
+        
+        //determine individual tile dimensions
         this.tileWidth = this.sliderWidth / this.numRows;
         this.tileHeight = this.sliderHeight / this.numRows;
         
         //add tiles (divs) to slider
-        var slider = document.getElementById('sliderWrapper');
+        var puzzle = document.getElementById('puzzleWrapper');
         for (var i = 0; i < this.numTiles; i++) {
             var newTile = document.createElement('div');
             newTile.id = 'tile' + i;
-            newTile.className = 'sliderTile';
+            newTile.className = 'puzzleTile';
             newTile.style.width = this.tileWidth + 'px';
             newTile.style.height = this.tileHeight + 'px';
-            slider.appendChild(newTile);
+            puzzle.appendChild(newTile);
         }//end for
          
-    }
+    },
     
-   /* gridSetup: function () {
-            
-        }*/
+   gridSetup: function () {
+       
+       //Create array of tiles because
+       //getElementsByClassName result isn't actually an array
+       var tiles = document.getElementsByClassName('puzzleTile');
+       var tilesArray = [];
+   
+       for (var i = 0; i < tiles.length; i++) {
+           tilesArray.push(tiles[i]);
+       }
+       
+       //Create grid rows and place / position tiles in each row
+       for (i = 0; i < this.numRows; i++) {
+           this.grid['row' + i] = tilesArray.splice(0, 3);
+          
+           var row = this.grid['row' + i];
+           var yPos = 0;
+           var xPos = 0;
+           
+           //add x position to tiles
+           
+           
+           //add y position to tiles
+           for (var j = 0; j < row.length; j++) {
+                row[j].yPos_final = yPos;
+                row[j].style.top = yPos + 'px';
+                yPos = yPos + this.tileHeight;
+            }
+           
+        }//end grid creation for loop
+       
+   }
     
 };//end setSlider
 
-setSlider.sliderDimensions();
-setSlider.tileDimensions();
+slidePuzzle.puzzleSize();
+slidePuzzle.createTiles();
+slidePuzzle.gridSetup();
